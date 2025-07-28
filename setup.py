c8248c4c9fc39b3d5a640c1a7017b51bb589797f
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-07-28 14:00:20 +0100

    packaging

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='CIMEmoji',
+setup(name='cim_emoji',
       version='0.1',
       description='Exploring emojis and text',
       author='Iain Emsley',
       author_email='iain.emsley@warwick.ac.uk',
       url='',
-      packages=['CIMEmoji'],
+      packages=['cim_emoji'],
      )
